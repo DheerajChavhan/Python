2c02e8fc0b5a38ceeb050ea3732b03f2670a84eb
DheerajChavhan <dheerajchavhan15@gmail.com>
2025-09-06 00:21:41 +0530

    Add files via upload

---
+++ CHAPTER_02/01_Variables.py
@@ -0,0 +1,6 @@
+a=5
+b=6.22
+print(a+b)
+
+c="Dheeraj"
+print(c)
---
+++ CHAPTER_02/02_Datatypes.py
@@ -0,0 +1,5 @@
+a=1 # a is a integer
+b=1.23 # b is a decimal number
+c="Dheeraj" # c is a string
+d=False # d is a boolean type datatype
+e= None # e is none type datatype
