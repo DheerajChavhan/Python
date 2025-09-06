6100d619100b15c97d6d5821b629ebce72960fd9
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-06 13:28:26 +0530

    Add files via upload

---
+++ CHAPTER_02/03_Operators.py
@@ -0,0 +1,39 @@
+# Arithmatic Operator
+a=4
+b=6
+c=a+b 
+print(c)
+
+# Assignment Operator
+a=4-2  # Assign the value of 4-2 in a
+print(a)
+b=3
+b+=3 # Increment the value of b by 3 and then assign in b
+print(b)
+
+# Comaparison Opertors 
+
+d=5<4 #Always return boolean values
+e=4>2
+f = 5<=5
+g=6>=7
+h=5!=5
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+# Logical Operators
+
+x= True or False
+
+# Truth table of "AND"
+print("True and False is:", True and False)
+print("True and True is:", True and True)
+print("False and False is:", False and False)
+
+# Truth table of "OR"
+print("True or False is:", True or False)
+print("True or True is:", True or True)
+print("False or False is:", False or False)
---
+++ CHAPTER_02/04_Type.py
@@ -0,0 +1,5 @@
+a="5"
+b=float(a) # Convert the type of a in float
+type(a) # Type of variable
+print(type(a)) 
+print(type(b))
