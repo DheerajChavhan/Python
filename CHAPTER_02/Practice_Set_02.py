9412edccd3cb4c187f3092b4a367ceddea772051
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-07 12:23:53 +0530

    Add files via upload

---
+++ CHAPTER_02/Practice_Set_02.py
@@ -0,0 +1,36 @@
+# Write program to add two numbers
+
+a=int(input("Enter the first number:", ))
+b=int(input("Enter the second number number:", ))
+print("The sum is:", a+b)
+
+# Write a program to find the remainder when it is divided by z
+
+c=int(input("Enter the number:", ))
+d=int(input("Enter the z:", ))
+e=c%d
+print("The remainder is:", e)
+
+#Check the type of variable using input function
+
+f=input("Enter the variable that needs to be check:", )
+print (f)
+g=type(f)
+print("The data type of entered variable is:", g)
+
+#Use comparator to find whether a is greater than b or not (take a=34 & b=80)
+
+a=int(input("Enter the number 'a' :", ))
+b=int(input("Enter the number 'b' :", ))
+print("a is greater than b is:", a>b)
+
+# find the average of two numbers enterd by user
+
+a=int(input("Enter the first number:", ))
+b=int(input("Enter the second number number:", ))
+c=print("The average of two numbers is:", (a+b)/2)
+
+# find the square of number enterd by user
+
+a=int(input("Enter the number:", ))
+print("The square of the number is:", a**3)
