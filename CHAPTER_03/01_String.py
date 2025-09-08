4e61c7bcd7a53c918b078501b8d4510275b06b31
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-08 17:11:22 +0530

    Add files via upload

---
+++ CHAPTER_03/01_String.py
@@ -0,0 +1,9 @@
+name="Dheeraj"
+print(len(name))  #Prints the length of the string
+nameshort=name[0:6] #starts from index 0 till index 2 excluding 2
+print(nameshort)
+character=name[4] #Prints the index 4
+print(character)
+print(name[:4]) #This means starting is from 0 
+print(name[2:]) #This means starting from index 2 to last including last
+print(name[1:5:3]) #first element is of first index then index 1 till index 4 are selected and from that element index 3 is chosen
---
+++ CHAPTER_03/02_Str_functions.py
@@ -0,0 +1,5 @@
+name="Dheeraj"
+print(len(name))
+print(name.endswith("raj"))
+print(name.startswith("dhe")) # Capital H and small h is different
+print(name.capitalize()) # Capitalize the first letter of the string
