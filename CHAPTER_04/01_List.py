75effb0c0d33136bec374c0de7c6fe5e71ed7531
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-10 00:18:20 +0530

    Add files via upload

---
+++ CHAPTER_04/01_List.py
@@ -0,0 +1,4 @@
+friends=["Dheeraj",45,"Shravan",45.7,True]  #It can store any datatype
+print(friends[3])
+friends[3]=100 #Unlike strings lists are mutable
+print(friends)
---
+++ CHAPTER_04/02_list_methods.py
@@ -0,0 +1,18 @@
+friends=["Dheeraj",45,"Shravan",45.7,True] 
+print(friends)
+
+friends.append("Prem") #insert value at last
+print(friends)
+
+numbers=[1,4,2,6,0]
+numbers.reverse()
+print(numbers)
+
+numbers.sort()
+print(numbers)
+numbers.insert(3,10) #insert 10 at index 3
+print(numbers)
+numbers.pop(3)
+print(numbers) #remove element from index 3
+numbers.remove(6) #remove the element 6 from wherever it is
+print(numbers) 
