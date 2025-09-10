810a0d436ca2161de724d099a5e12e913305743b
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-10 23:10:04 +0530

    Add files via upload

---
+++ CHAPTER_04/04_Tuple_methods.py
@@ -0,0 +1,5 @@
+a=(1,12,13,14,15,12)
+print(a.count(12))   #print the no of times 12 occured
+print(a.index(12))   #print the index of first 12 in the tuple
+print(12 in a)       #check if 12 is present and returns True/False
+print(len(a))        #print the length of a
---
+++ CHAPTER_04/Practice_Set_04.py
@@ -0,0 +1,40 @@
+#Write a program to store seven fruits entered by user
+# a=input("Enter the first fruit:", )
+# b=input("Enter the second fruit:", )
+# c=input("Enter the third fruit:", )
+# d=input("Enter the fourth fruit:", )
+# e=input("Enter the fifth fruit:", )
+# f=input("Enter the sixth fruit:", )
+# g=input("Enter the seventh fruit:", )
+
+# Fruits=(a,b,c,d,e,f,g)
+# print("The fruits are:",Fruits)
+
+#Write a program to store Marks of 6 students entered by user in a sorted manner
+# a=int(input("Enter the Marks of student:", ))
+# b=int(input("Enter the Marks of student:", ))
+# c=int(input("Enter the Marks of student:", ))
+# d=int(input("Enter the Marks of student:", ))
+# e=int(input("Enter the Marks of student:", ))
+# f=int(input("Enter the Marks of student:", ))
+# g=int(input("Enter the Marks of student:", ))
+
+# Marks=[a,b,c,d,e,f,g]
+# Marks.sort()
+# print("The Marks are:",Marks)
+
+#Check that a type cannot be changed in python
+
+# x=(12,"Dheeraj",23.4)
+# x[2]=7   # Tuple Can't be changed because it is immutable
+# print(x)
+
+#Write a program to sum a list with four numbers
+
+# a=[2,3,4,5]
+# print(sum(a))
+
+#write a program to count the number of zeros in a=(7,0,8,0,0,9)
+
+a=(7,0,8,0,0,9)
+print(a.count(0))
