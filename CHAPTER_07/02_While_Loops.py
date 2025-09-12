f768ce169eb25dc71c61bbd53503f12d2c887c8f
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-13 02:21:53 +0530

    Add files via upload

---
+++ CHAPTER_07/02_While_Loops.py
@@ -0,0 +1,13 @@
+i=1
+while(i<11):
+    print(i)
+    i=i+1
+ 
+
+# write all the elements of list using while loop
+
+L=[1,"Dheeraj","Prem","Abhiman","Suraj",6]
+i=0
+while(len(L)>i):
+    print(L[i])
+    i+=1
