34e99130cd6481c737cf53014aa657c7885d4436
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-14 00:41:33 +0530

    Add files via upload

---
+++ CHAPTER_07/03_For_Loops.py
@@ -0,0 +1,26 @@
+for i in range(5):  
+    print(i)
+    
+for i in range(0,100,4):  #Start,end,stepsize
+    print(i)
+
+L=[1,2,5,3,6,8,34,654]
+
+for i in L:
+    print(i)
+
+T = ("Dheeraj",12,"Prem")
+
+for i in T:
+    print(i)
+
+for i in range(1,100):
+    if(i==5):
+        continue   #it will skip the value of i inside the if
+    print(i)
+
+for i in range(1,100):
+    if(i==5):
+        break  #it will skip all the value of i after the if value
+    print(i)
+
