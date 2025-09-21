8a89f4edaded808976f394fa9f8c07083c61d7d0
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-21 11:53:26 +0530

    Add files via upload

---
+++ CHAPTER_10/01_Class.py
@@ -0,0 +1,8 @@
+class Employee:
+    Name="Dheeraj"  # This is a class attribute
+    Language="Py"
+    Salary=100000
+
+A=Employee()
+A.place="Pune"      #This is a object/instance attribute
+print(A.place,A.Name,A.Salary)
