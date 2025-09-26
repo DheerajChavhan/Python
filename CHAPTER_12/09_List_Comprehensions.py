d0631aae48ffb52fc53ff3906ce7f4070e422f0a
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-27 05:17:42 +0530

    Add files via upload

---
+++ CHAPTER_12/09_List_Comprehensions.py
@@ -0,0 +1,9 @@
+List=[1,2,3,4,5,6]
+
+# SquaredList=[]
+# for item in List:                  #This can simplified by using the following method
+#     SquaredList.append(item*item)
+
+SquaredList=[i*i for i in List]
+
+print(SquaredList) 
