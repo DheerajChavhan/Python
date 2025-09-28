dda581991a9d27eef4dd58c335dff69abd342088
Dheeraj_Chavhan <dheerajchavhan15@gmail.com>
2025-09-29 01:58:08 +0530

    Add files via upload

---
+++ CHAPTER_13/Practice_Set_13.py
@@ -0,0 +1,18 @@
+# Q1:Write a program to input name,marks and phone number of a student and format it using the format function like below:
+# The name of the student is Dheeraj , His marks are 100 and phone number is 9876543210
+
+# name=input("Enter the name:")
+# marks=int(input("Enter the marks:"))
+# Phone_No=int(input("Enter Your Phone number:"))
+
+# A="The name of the Student is {}\nHis Marks are:{}\nHis Phone number is:{}".format(name,marks,Phone_No)
+# print(A)
+
+
+#Q2: A list contains a mutiplication of 7. write the program to write the vertical string of same numbers
+
+# L=[str(7*i) for i in range(1,11)]
+# M="\n".join(L)
+# print(M)
+
+
